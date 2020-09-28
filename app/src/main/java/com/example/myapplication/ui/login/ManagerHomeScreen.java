7351c3e43f999cda455b78751dae22580076ca2a
Arecobaleno <674407238@qq.com>
2020-09-28 11:07:15 +0800

    sqlite and login

---
+++ app/src/main/java/com/example/myapplication/ui/login/ManagerHomeScreen.java
@@ -0,0 +1,6 @@
+package com.example.myapplication.ui.login;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ManagerHomeScreen extends AppCompatActivity {
+}
---
+++ app/src/main/java/com/example/myapplication/ui/login/OperatorHomeScreen.java
@@ -0,0 +1,6 @@
+package com.example.myapplication.ui.login;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class OperatorHomeScreen extends AppCompatActivity {
+}
